540321cc44dff1883b1c3514ec7d33a1b2309253
macabolarin <macabolarin@gmail.com>
2026-01-23 01:18:14 +0100

    add python class

--- hello.py
+++ hello.py
@@ -1,42 +1,94 @@
-a = 200
-b = 33
-
+a = 33
+b = 333
 if b > a:
     print("b is greater than a")
+
+    number = 15
+    if number > 0:
+        print("the number is positive")
+
+age = 18
+if age >= 18:
+    print("ypu are eligible")
+    print("you can vote")
+    print("you are legally right")
+    print("you are enfranchised")
+
+is_logged_in = True
+if is_logged_in:
+    print("welcome back")
+
+a = 33
+b =33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+score = 75
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade B")
+elif score >= 70:
+    print("Grade: c")
+elif score >= 60:
+    print("Grade: D")
+
+age = 25
+
+if age < 13:
+    print("you are a child")
+elif age < 20:
+    print("you are a teenager")
+elif age < 60:
+    print("you are an adult")
+elif age < 65:
+    print("you are a senior")
+
+number = 7
+if number % 2 ==0:
+    print("the number is even")
 else:
-    print("b is less than a")
+    print("the number iis odd")
 
-x = "hello"
-y = 15
-print(bool(x))
-print(bool(y))
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+    print("both conditions are true")
 
-bool("abc")
-bool(123)
-bool(["apple", "banana", "orange"])
+username = "drake"
+password = "const"
+is_verified = True
 
-def myfunction() :
-    return True
-print(myfunction())
-if myfunction():
-    print("yes")
+if username and password and is_verified:
+    print("login successful")
 else:
-    print("NO!")
+    print("login failed")
 
-animal = ["monkey", "cow", "rabbit"]
-print("rabbit" in animal)
+username = "mich"
+password = "mich123"
+is_active = True
 
-thislist = ["monkey", "cow", "goat"]
-print(len(thislist))
+if username:
+    if password:
+        if is_active:
+            print("login successful")
+        else:
+            print("account is not active")
+    else:
+        print("password required")
+else:
+    print("username required")
 
-thislistt = ["goat", "cow", "monkey"]
-if "goat" in thislist:
-    print("yes, 'goat' is in the list")
+a = 33
+b = 330
+if b > a:
+    pass
 
-thislist = ["goat", "monkey", "cow"]
-thislist.sort()
-print(thislist)
-
-thislist = ["10", "20", "30", "70", "23"]
-thislist.sort()
-print(thislist)
+age = 16
+if age < 18:
+    pass
+else:
+    print("access granted")
