287c439e32527e510e7dc693effe383ebb317890
macabolarin <macabolarin@gmail.com>
2026-01-23 23:19:26 +0100

    add python

--- hello.py
+++ hello.py
@@ -12,3 +12,27 @@ def myfunc():
         print(x)
     myinnerfunc()
 myfunc()
+
+x = 300
+
+def myfunc():
+    x = 200
+    print(x)
+myfunc()
+print(x)
+
+def myfunc():
+    global x
+    x = 300
+myfunc()
+print(x)
+
+x = range(10)
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
