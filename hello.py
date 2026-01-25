2f5e26a00bd6801dd18e9b818877c63d058f33ab
macabolarin <macabolarin@gmail.com>
2026-01-25 23:45:38 +0100

    add python file

--- hello.py
+++ hello.py
@@ -1,46 +1,33 @@
-def my_function():
-    print("hello")
-my_function()
+inflation = 330
+interest = 222
 
-def get_greetings():
-    return "hello there from funtion"
-print(get_greetings())
+if inflation > interest:
+    print("good")
+else:
+    print("bad")
 
-def myfunc():
-    x = 300
-    def myinnerfunc():
-        print(x)
-    myinnerfunc()
-myfunc()
+Gdp = 20
+Vat = 7.5
+interest = 19
+minimumwage = 70000
+tax = 30
 
-x = 300
+if Gdp > Vat:
+    print("good")
+else:
+    print("bad")
 
-def myfunc():
-    x = 200
-    print(x)
-myfunc()
-print(x)
+if Gdp >  interest:
+    print("good")
+else:
+    print("bad")
 
-def myfunc():
-    global x
-    x = 300
-myfunc()
-print(x)
+if Gdp > minimumwage:
+    print("good")
+else:
+    print("bad")
 
-x = range(10)
-
-x = min(5, 10, 25)
-y = max(5, 10, 25)
-
-print(x)
-
-print("enter your name:")
-name = input()
-print(f'hello{name}')
-
-name = input("enter your name:")
-print(f"hello{name}")
-fav1 = input("what is your favorite animal:")
-fav2 = input("what is your favorite colour:")
-fav3 = input("what is your favorite number:")
-print(f"do you want a {fav2} {fav1} with {fav3} legs?")
+if Gdp > tax:
+    print("good")
+else:
+    print("bad")
