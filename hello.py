f0d25f6bd19ce29531fd7ab9eea274e3ff684a76
macabolarin <macabolarin@gmail.com>
2026-01-25 23:58:38 +0100

    add python file

--- hello.py
+++ hello.py
@@ -31,3 +31,18 @@ if Gdp > tax:
     print("good")
 else:
     print("bad")
+
+face = "recognised"
+
+if face != "recognised":
+    print("not recognised, cant vote")
+else:
+    print("face recognised, accredit and vote")
+
+fingerprint = "match"
+
+if fingerprint == "match":
+    print("fingerprint match and voted")
+else:
+    print("not matched, use facial recognition")
+
